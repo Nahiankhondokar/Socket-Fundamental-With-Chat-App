6d946e144ab7a98da929302b7ad4b68fcc65c02e
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-10-29 14:35:29 +0600

    html file load

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const colors = require('colors');
+const path = require('path');
+const { createServer } = require('http');
+const {} = require('socket.io');
 
 
 // init express
 const app = express();
 
-
+// route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client.html'));
+});
 
 
 app.listen(5050, () => {
