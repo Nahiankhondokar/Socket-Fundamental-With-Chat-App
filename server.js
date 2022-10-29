37b4a685165c89f4542fbba8b8aa1f449464872a
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-10-29 14:43:58 +0600

    express to row to socket server done

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const colors = require('colors');
 const path = require('path');
 const { createServer } = require('http');
-const {} = require('socket.io');
+const { Server } = require('socket.io');
 
 
 // init express
 const app = express();
+
+
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+// create express server to row server
+const httpServer = createServer(app);
+// row server to socket server
+const io = new Server(httpServer);
+
+
+
 
 // route
 app.get('/', (req, res) => {
